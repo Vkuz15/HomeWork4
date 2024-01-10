0349392761e94f6c64e8efa8f68e5384d15fca57
Владимир <vkuznecov1504@mail.ru>
2024-01-10 12:28:53 +0400

    Инициализировали переменную temperature Добавили условный оператор if с двумя действиями

--- Main.java
+++ Main.java
@@ -9,5 +9,14 @@ public class Main {
         } else {
             System.out.println("Если возраст человека равен " + age + " то, он не достиг совершеннолетия, нужно немного подождать");
         }
+
+        // Задача 2
+
+        int temperature = 7;
+        if (temperature <= 5) {
+            System.out.println("На улице холодно, нужно надеть шапку");
+        } else {
+            System.out.println("Сегодня тепло, можно идти без шапки");
+        }
     }
 }
