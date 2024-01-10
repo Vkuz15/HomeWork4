95a6c1e4e834ed0073211c00d4d1cf871a2537de
Владимир <vkuznecov1504@mail.ru>
2024-01-10 12:13:40 +0400

    Инициализировали переменную age Добавили условный оператор if с двумя действиями

--- Main.java
+++ Main.java
@@ -1,4 +1,13 @@
 public class Main {
     public static void main(String[] args) {
+
+        // Задача 1
+
+        int age = 17;
+        if (age >= 18) {
+            System.out.println("Если возраст человека равен " + age + " то, он совершеннолетний");
+        } else {
+            System.out.println("Если возраст человека равен " + age + " то, он не достиг совершеннолетия, нужно немного подождать");
+        }
     }
 }
